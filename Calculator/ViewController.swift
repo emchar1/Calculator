b72c45bc123e6d1da7c6a99a6b209b16da4f5bcc
Eddie Char <emchar1@gmail.com>
2020-09-03 01:52:27 -0700

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Eddie Char on 9/3/20.
+//  Copyright © 2020 Eddie Char. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
