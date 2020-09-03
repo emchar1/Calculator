b5fbfbac295f70b891d8e7f86b1b457ea61df61e
Eddie Char <emchar1@gmail.com>
2020-09-03 12:13:25 -0700

    Add rainbow buttons.

---
+++ Calculator/Views/numberButton.swift
@@ -0,0 +1,21 @@
+//
+//  numberButton.swift
+//  Calculator
+//
+//  Created by Eddie Char on 9/3/20.
+//  Copyright © 2020 Eddie Char. All rights reserved.
+//
+
+import UIKit
+
+class numberButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
